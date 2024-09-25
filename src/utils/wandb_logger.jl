1237c7646de9205f24a405700ea464febdf6aad5
Emil Warnberg <ewarnberg@g.harvard.edu>
2024-09-25 10:29:27 -0400

    Quantile function needed for previous commit

--- src/utils/wandb_logger.jl
+++ src/utils/wandb_logger.jl
@@ -3,12 +3,17 @@ import CUDA
 
 function log_to_wandb(lg::Wandb.WandbLogger, key, val; quantiles=0:0.25:1)
     if ndims(val) >= 1
-        if length(val) > 0
-            inds = Int.(ceil.(CUDA.cu(quantiles) .* (length(val) .- 1))) .+ 1
-            qv = CUDA.sort(view(val, :))[inds] |> Array
-            Wandb.log(lg, Dict("$(key)_q$(100q)" => v for (q,v) in zip(quantiles, qv)), commit=false)
-        end
+        Wandb.log(lg, quantile_dict(key, val), commit=false)
     else
         Wandb.log(lg, Dict(key=>val), commit=false)
     end
+end
+
+function quantile_dict(prefix, val; quantiles=0:0.25:1)
+    if length(val) == 0
+        return Dict{String, eltype(val)}()
+    end
+    inds = Int.(ceil.(CUDA.cu(quantiles) .* (length(val) .- 1))) .+ 1
+    qv = CUDA.sort(view(val, :))[inds] |> Array
+    Dict("$(prefix)_q$(100q)" => v for (q,v) in zip(quantiles, qv))
 end
