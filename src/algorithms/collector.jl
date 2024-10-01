80688b4623334c6f49a2fcc021a0902c399a61d8
Emil Warnberg <ewarnberg@g.harvard.edu>
2024-10-01 18:12:14 -0400

    First attempt at multi-node with MPI

--- src/algorithms/collector.jl
+++ src/algorithms/collector.jl
@@ -1,8 +1,9 @@
 include("../utils/component_tensor.jl")
 
-function collect_batch(envs, actor_critic, params)
+function collect_batch_root(envs, actor_critic, params)
     steps_per_batch = params.rollout.n_steps_per_epoch
-    n_envs = length(envs)
+    n_envs = params.rollout.n_envs
+    n_local_envs = length(envs)
 
     #Big GPU arrays/BatchComponentTensor for storing the entire batch
     template_state = state(envs[1], params)
@@ -20,37 +21,98 @@ function collect_batch(envs, actor_critic, params)
     step_states = BatchComponentTensor(template_state, n_envs)
     step_reward = zeros(Float32, n_envs)
     step_status = zeros(UInt8, n_envs)
+    step_info  = BatchComponentTensor(template_info, n_envs)
+
+    local_step_states = BatchComponentTensor(template_state, n_local_envs)
+    local_step_reward = zeros(Float32, n_local_envs)
+    local_step_status = zeros(UInt8, n_local_envs)
+    local_step_infos  = BatchComponentTensor(template_info, n_local_envs)
 
     #States for the first timestep
-    @Threads.threads for i=1:n_envs
+    @Threads.threads for i=1:n_local_envs
         if params.rollout.reset_on_epoch_start
             reset!(envs[i], params)
         end
-        step_states[:, i] = state(envs[i], params)
+        local_step_states[:, i] = state(envs[i], params)
     end
+    MPI.Gather!(local_step_states |> data, step_states |> data, MPI.COMM_WORLD)
+
     states[:, :, 1] = step_states
     for t=1:steps_per_batch
         actor_output[:, :, t] = actor(actor_critic, view(states, :, :, t), params) |> ComponentTensor
         step_actions = Array(view(actor_output, :action, :, t))
-        @Threads.threads for i=1:n_envs
+        local_step_actions = zeros(eltype(step_actions), size(step_actions, 1), n_local_envs)
+        MPI.Scatter!(step_actions, local_step_actions, MPI.COMM_WORLD)
+        @Threads.threads for i=1:n_local_envs
             env = envs[i]
-            action = view(step_actions, :, i)
+            action = view(local_step_actions, :, i)
             act!(env, action, params)
-            step_states[:, i] = state(env, params)
-            step_reward[i] = reward(env, params)
-            step_status[i] = status(env, params)
-            infos[:, i, t] = info(env)
-            if step_status[i] != RUNNING
+            local_step_states[:, i] = state(env, params)
+            local_step_reward[i] = reward(env, params)
+            local_step_status[i] = status(env, params)
+            local_step_infos[:, i] = info(env)
+            if local_step_status[i] != RUNNING
                 reset!(env, params)
             end
         end
+        MPI.Gather!(local_step_states |> data, step_states |> data, MPI.COMM_WORLD)
+        MPI.Gather!(local_step_infos |> data, step_info |> data, MPI.COMM_WORLD)
+        MPI.Gather!(local_step_reward, step_reward, MPI.COMM_WORLD)
+        MPI.Gather!(local_step_status, step_status, MPI.COMM_WORLD)
         #Move the CPU arrays to the correct index of the bigger GPU arrays
         states[:, :, t+1] = step_states
+        infos[:, :, t] = step_info
         rewards[:, t] = step_reward
         env_status[:, t] = step_status
     end
 
     return (;states, actor_output, rewards, status=env_status, infos)
+end
+
+function collect_batch_worker(envs, params)
+    action_size =  mapreduce(s->length(s), +, null_action(envs[1], params))
+
+    steps_per_batch = params.rollout.n_steps_per_epoch
+    n_envs = params.rollout.n_envs
+    n_local_envs = length(envs)
+    template_state = state(envs[1], params)
+    template_info = info(envs[1])
+    local_step_states = BatchComponentTensor(template_state, n_local_envs)
+    local_step_reward = zeros(Float32, n_local_envs)
+    local_step_status = zeros(UInt8, n_local_envs)
+    local_step_infos  = BatchComponentTensor(template_info, n_local_envs)
+
+    #States for the first timestep
+    @Threads.threads for i=1:n_local_envs
+        if params.rollout.reset_on_epoch_start
+            reset!(envs[i], params)
+        end
+        local_step_states[:, i] = state(envs[i], params)
+    end
+    MPI.Gather!(local_step_states |> data, nothing, MPI.COMM_WORLD)
+
+    for t=1:steps_per_batch
+        local_step_actions = zeros(Float32, action_size, n_local_envs)
+        MPI.Scatter!(nothing, local_step_actions, MPI.COMM_WORLD)
+        @Threads.threads for i=1:n_local_envs
+            env = envs[i]
+            action = view(local_step_actions, :, i)
+            act!(env, action, params)
+            local_step_states[:, i] = state(env, params)
+            local_step_reward[i] = reward(env, params)
+            local_step_status[i] = status(env, params)
+            local_step_infos[:, i] = info(env)
+            if local_step_status[i] != RUNNING
+                reset!(env, params)
+            end
+        end
+        MPI.Gather!(local_step_states |> data, nothing, MPI.COMM_WORLD)
+        MPI.Gather!(local_step_infos |> data, nothing, MPI.COMM_WORLD)
+        MPI.Gather!(local_step_reward, nothing, MPI.COMM_WORLD)
+        MPI.Gather!(local_step_status, nothing, MPI.COMM_WORLD)
+    end
+
+    return nothing
 end
 
 function compute_batch_stats(batch)
--- src/run_ppo.jl
+++ src/run_ppo.jl
@@ -1,39 +1,61 @@
-using Flux
-using ProgressMeter
 import Dates
-import BSON
+import MPI
+println("[$(Dates.now())] Initalizing MPI...")
+MPI.Init(threadlevel=:funneled)
 include("environments/rodent_imitation_env.jl")
 include("algorithms/collector.jl")
-include("algorithms/ppo_loss.jl")
-include("algorithms/ppo_networks.jl")
-include("utils/wandb_logger.jl")
 include("params.jl")
+if MPI.Comm_rank(MPI.COMM_WORLD)==0
+    using Flux
+    using ProgressMeter
+    import BSON
+    include("algorithms/ppo_loss.jl")
+    include("algorithms/ppo_networks.jl")
+    include("utils/wandb_logger.jl")
+end
 
 function run_ppo(params)
+    @assert MPI.Is_thread_main()
+    mpi_rank = MPI.Comm_rank(MPI.COMM_WORLD)
+    mpi_size = MPI.Comm_size(MPI.COMM_WORLD)
+    @assert params.rollout.n_envs % mpi_size == 0
     test_env = RodentImitationEnv(params)
-    actor_critic = ActorCritic(test_env, params) |> Flux.gpu
-    opt_state = Flux.setup(Flux.Adam(params.training.learning_rate), actor_critic)
-    envs = [clone(test_env, params) for _=1:params.rollout.n_envs]
-    starttime = Dates.now()
-    run_name = "TestCheckpointLogging-$(starttime)" #ImitationWithAppendages
-    lg = Wandb.WandbLogger(project = "Rodent-Imitation", name = run_name, config = params_to_dict(params))
-    mkdir("runs/checkpoints/$(run_name)")
-    @showprogress for epoch = 1:params.rollout.n_epochs
-        batch = collect_batch(envs, actor_critic, params)
-        ppo_log = ppo_update!(batch, actor_critic, opt_state, params)
+    n_local_envs = params.rollout.n_envs ÷ mpi_size
+    
+    envs = [clone(test_env, params) for _=1:n_local_envs]
 
-        logdict = compute_batch_stats(batch)
-        merge!(logdict, ppo_log)
-        logdict["total_steps"] = epoch * params.rollout.n_envs * params.rollout.n_steps_per_epoch
+    if mpi_rank == 0
+        actor_critic = ActorCritic(test_env, params) |> Flux.gpu
+        opt_state = Flux.setup(Flux.Adam(params.training.learning_rate), actor_critic)
+        
+        starttime = Dates.now()
+        run_name = "TestMPI-$(starttime)" #ImitationWithAppendages
+        lg = Wandb.WandbLogger(project = "Rodent-Imitation", name = run_name, config = params_to_dict(params))
+        mkdir("runs/checkpoints/$(run_name)")
+        println("[$(Dates.now())] Root ready...")
+        @showprogress for epoch = 1:params.rollout.n_epochs
+            batch = collect_batch_root(envs, actor_critic, params)
+            ppo_log = ppo_update!(batch, actor_critic, opt_state, params)
 
-        if epoch % params.training.checkpoint_interval == 0
-            checkpoint_fn = "runs/checkpoints/$(run_name)/step-$(epoch).bson"
-            BSON.bson(checkpoint_fn; actor_critic=Flux.cpu(actor_critic))
-            lg.wrun.log_model(checkpoint_fn, "checkpoint-step-$(epoch).bson")
+            logdict = compute_batch_stats(batch)
+            merge!(logdict, ppo_log)
+            logdict["total_steps"] = epoch * params.rollout.n_envs * params.rollout.n_steps_per_epoch
+
+            if epoch % params.training.checkpoint_interval == 0
+                checkpoint_fn = "runs/checkpoints/$(run_name)/step-$(epoch).bson"
+                BSON.bson(checkpoint_fn; actor_critic=Flux.cpu(actor_critic))
+                lg.wrun.log_model(checkpoint_fn, "checkpoint-step-$(epoch).bson")
+            end
+            Wandb.log(lg, logdict)
         end
-        Wandb.log(lg, logdict)
+        Wandb.close(lg);
+    else
+        println("[$(Dates.now())] Worker $mpi_rank ready...")
+        for epoch = 1:params.rollout.n_epochs
+            collect_batch_worker(envs, params)
+        end
     end
-    Wandb.close(lg);
 end
 
 run_ppo(params)
+MPI.Finalize()
