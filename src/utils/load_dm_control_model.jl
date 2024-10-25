6e7d5edf9b4c92263f4509bd767fd9cba134e781
Emil Warnberg <ewarnberg@g.harvard.edu>
2024-10-25 16:54:44 -0400

    Changed the model loader to ues PythonCall instead of PyCall

--- src/utils/load_dm_control_model.jl
+++ src/utils/load_dm_control_model.jl
@@ -1,11 +1,12 @@
-import PyCall
+import PythonCall
 import MuJoCo
-const dm_locomotion = PyCall.pyimport("dm_control.locomotion")
+const dm_locomotion = PythonCall.pyimport("dm_control.locomotion")
 for submod in ("walkers", "arenas", "tasks")
-    PyCall.pyimport("dm_control.locomotion.$submod")
+    PythonCall.pyimport("dm_control.locomotion.$submod")
 end
 
-function dm_control_rodent(;torque_actuators=true, foot_mods=true, scale=1.0,
+function dm_control_rodent(;torque_actuators=true,
+                            foot_mods=true, scale=1.0,
                             physics_timestep=0.002, control_timestep=0.02)
     walker = dm_locomotion.walkers.rodent.Rat(;torque_actuators, foot_mods)
     dm_locomotion.walkers.rescale.rescale_subtree(walker.root_body, scale, scale)
@@ -20,10 +21,12 @@ function dm_control_rodent(;torque_actuators=true, foot_mods=true, scale=1.0,
     mjcf_model = task.root_entity.mjcf_model
     modelfolder = mktempdir()
     mjcf_model_file = "$modelfolder/mjcf_model.xml"
-    write(mjcf_model_file, mjcf_model.to_xml_string())
+    write(mjcf_model_file, PythonCall.pyconvert(String, mjcf_model.to_xml_string()))
     #Put skins, textures, etc in the same temp folder
-    for (file_name, file_contents) in pairs(mjcf_model.get_assets())
-        write("$modelfolder/$file_name", file_contents)
+    for file_name in mjcf_model.get_assets()
+        file_contents = mjcf_model.get_assets()[file_name]
+        as_julia_buffer = PythonCall.pyconvert(Vector{UInt8}, file_contents)
+        write("$modelfolder/$file_name", as_julia_buffer)
     end
     return MuJoCo.load_model(mjcf_model_file)
 end
