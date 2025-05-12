a55f5681ed3e1c178a7b061ef65df2a5a0545be4
Emil Warnberg <ewarnberg@g.harvard.edu>
2025-05-12 12:30:43 -0400

    Commit of configs recently used on the cluster (but see WandB for overrides in individual runs).

--- submit_mpi.sh
+++ submit_mpi.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 #SBATCH -J rodentImitation
-#SBATCH --ntasks=1 --cpus-per-task 32 --mem-per-cpu 1000 -p gpu -t 3-00:00 -o %j_gpu.out -e %j_gpu.err --gres=gpu
+#SBATCH --ntasks=1 --cpus-per-task 32 --mem-per-cpu 1000 -p gpu -t 3-00:00 -o %j_gpu.out -e %j_gpu.err --gres=gpu --constraint="holyhdr|holyndr" --no-requeue
 #SBATCH hetjob
-#SBATCH --ntasks=15 --cpus-per-task 32 --mem-per-cpu 1000 -p sapphire,shared -t 3-00:00 -o %j.out -e %j.err
+#SBATCH --ntasks=15 --cpus-per-task 32 --mem-per-cpu 1000 -p sapphire,shared -t 3-00:00 -o %j.out -e %j.err --constraint="holyhdr|holyndr" --no-requeue
 
 module add intel
 module add openmpi
+export JULIA_PKG_PRECOMPILE_AUTO=0 #Precompilation is annoying with MPI
 export UCX_WARN_UNUSED_ENV_VARS=n
 export UCX_ERROR_SIGNALS="SIGILL,SIGBUS,SIGFPE"
 export JULIA_CPU_TARGET="sapphirerapids;skylake-avx512;cascadelake;icelake-server"
 
 srun --mpi=pmix -n 1 : -n 15\
      julia --threads=32 --project=. src/main.jl\
-     configs/imitation_sota.toml\
-     -wandb.run_name "Basic-MLP-again-{NOW}"\
-     -network.decoder_type "MLP"
+     configs/simplified_imitation.toml
+
+
