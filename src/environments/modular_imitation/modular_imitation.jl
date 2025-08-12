fac0b60adb0579bd4c9d10e394712bb84e91effe
Emil Warnberg <ewarnberg@g.harvard.edu>
2025-08-12 19:02:30 -0400

    Added future targets to modular imitation, and changed reward functions slightly.

--- src/environments/modular_imitation/modular_imitation.jl
+++ src/environments/modular_imitation/modular_imitation.jl
@@ -24,124 +24,38 @@ function ModularImitationEnv(walker; max_target_distance::Float64,
 end
 
 function state(env::ModularImitationEnv)
-    target = get_current_target(env)
-    relative_root_pos = torso_yawmat(env.walker) * (target.root_pose.pos - root_pos(env.walker))
-    relative_root_rot = subQuat(target.root_pose.quat, root_xquat(env.walker))
-    future_root_pos = torso_yawmat(env.walker) * (get_future_root_pos(env) - root_pos(env.walker))
-
-    prop = proprioception(env.walker)
-    target = target.proprioception
-    (
-        hand_L = (
-            proprioception = prop.hand_L,
-            imitation_target = (
-                pos = target.hand_L.egocentric_hand_pos,
-                wrist_angle = target.hand_L.wrist_angle,
-                finger_angle = target.hand_L.finger_angle,
-                xaxis = target.hand_L.xaxis,
-            ),
-        ),
-        arm_L = (
-            proprioception = prop.arm_L,
-            imitation_target = (
-                elbow_angle = target.arm_L.elbow_angle,
-                elbow_height = target.arm_L.elbow_height,
-            ),
-        ),
-        hand_R = (
-            proprioception = prop.hand_R,
-            imitation_target = (
-                pos = target.hand_R.egocentric_hand_pos,
-                wrist_angle = target.hand_R.wrist_angle,
-                finger_angle = target.hand_R.finger_angle,
-                xaxis = target.hand_R.xaxis,
-            ),
-        ),
-        arm_R = (
-            proprioception = prop.arm_R,
-            imitation_target = (
-                elbow_angle = target.arm_R.elbow_angle,
-                elbow_height = target.arm_R.elbow_height
-            ),
-        ),
-        foot_L = (
-            proprioception = prop.foot_L,
-            imitation_target = (
-                pos = target.foot_L.egocentric_foot_pos,
-                toe_angle = target.foot_L.toe_angle,
-                ankle_angle = target.foot_L.ankle_angle,
-                xaxis = target.foot_L.xaxis
-            ),
-        ),
-        leg_L = (
-            proprioception = prop.leg_L,
-            imitation_target = (
-                knee_angle = target.leg_L.knee_angle,
-                hip_height = target.leg_L.hip_height,
-            ),
-        ),
-        foot_R = (
-            proprioception = prop.foot_R,
-            imitation_target = (
-                pos = target.foot_R.egocentric_foot_pos,
-                toe_angle = target.foot_R.toe_angle,
-                ankle_angle = target.foot_R.ankle_angle,
-                xaxis = target.foot_R.xaxis
-            ),
-        ),
-        leg_R = (
-            proprioception = prop.leg_R,
-            imitation_target = (
-                knee_angle = target.leg_R.knee_angle,
-                hip_height = target.leg_R.hip_height,
-            ),
-        ),
-        torso = (
-            proprioception = prop.torso,
-            imitation_target = (
-                zaxis = target.torso.zaxis,
-                lumbar_bend   = target.torso.lumbar_bend,
-                lumbar_twist  = target.torso.lumbar_twist,
-                lumbar_extend = target.torso.lumbar_extend,
-                height_above_ground = target.torso.height_above_ground,
-            ),
-        ),
-        head = (
-            proprioception = prop.head,
-            imitation_target = (
-                xaxis = target.head.xaxis,
-                egocentric_pos = target.head.egocentric_pos,
-                mandible = target.head.mandible
-            ),
-        ),
-        root = (;
-            #proprioception = (;),
-            imitation_target = (
-                relative_pos = relative_root_pos,
-                future_root_pos = future_root_pos,
-                relative_rot = relative_root_rot
-            )
-        )
-    )
+    current_target = interpolated_target(env, env.target_timepoint[]) |> NamedTuple
+    future_target =  interpolated_target(env, env.target_timepoint[] + 0.1) |> NamedTuple
+    
+    #Transform root to egocentric coords
+    rot_mat = torso_yawmat(env.walker) 
+    current_target = merge(current_target, (;root=(pos = rot_mat * (current_target.root.pos - root_pos(env.walker)),
+                                                   rot = subQuat(current_target.root.rot, root_xquat(env.walker)))))
+    future_target = merge(future_target, (;root=(pos = rot_mat * (future_target.root.pos - root_pos(env.walker)),
+                                                 rot = subQuat(future_target.root.rot, root_xquat(env.walker)))))
+    prop = merge(proprioception(env.walker), (;root=(;dummy=0.0)))
+    full_state = map(prop, current_target, future_target) do p, ct, ft
+        (proprioception=p, current_target=ct, future_target=ft)       
+    end
+    return full_state
 end
 
 function compute_rewards(env::ModularImitationEnv)
     reward_shape(a, b) = exp(-(norm(a-b)/0.5))
     cosine_dist(a, b) = dot(a, b) / norm(a) / norm(b)
 
-    target = get_current_target(env)
-    root_dist = norm(target.root_pose.pos - root_pos(env.walker))
-    root_ang  = 2*acos(abs(clamp(target.root_pose.quat' * root_xquat(env.walker), -1, 1)))
+    target = interpolated_target(env, env.target_timepoint[])
+    root_dist = norm(target.root.pos - root_pos(env.walker))
+    root_ang  = 2*acos(abs(clamp(target.root.rot' * root_xquat(env.walker), -1, 1)))
 
     prop = proprioception(env.walker)
-    target = target.proprioception
     (
         hand_L = (
             finger_joint = reward_shape(prop.hand_L.finger_angle, target.hand_L.finger_angle),
             wrist_joint = reward_shape(prop.hand_L.wrist_angle, target.hand_L.wrist_angle),
             orientation = dot(prop.hand_L.xaxis, target.hand_L.xaxis),
             #elbow_height = reward_shape(prop.hand_L.elbow_height, target.hand_L.elbow_height),
-            hand_pos = reward_shape(prop.hand_L.egocentric_hand_pos, target.hand_L.egocentric_hand_pos),
+            hand_pos = reward_shape(prop.hand_L.egocentric_hand_pos, target.hand_L.pos),
         ),
         arm_L = (
             #hand_pos = reward_shape(prop.arm_L.egocentric_hand_pos, target.arm_L.egocentric_hand_pos),
@@ -155,7 +69,7 @@ function compute_rewards(env::ModularImitationEnv)
             wrist_joint = reward_shape(prop.hand_R.wrist_angle, target.hand_R.wrist_angle),
             orientation = dot(prop.hand_R.xaxis, target.hand_R.xaxis),
             #elbow_height = reward_shape(prop.hand_R.elbow_height, target.hand_R.elbow_height),
-            hand_pos = reward_shape(prop.hand_R.egocentric_hand_pos, target.hand_R.egocentric_hand_pos),
+            hand_pos = reward_shape(prop.hand_R.egocentric_hand_pos, target.hand_R.pos),
         ),
         arm_R = (
             #hand_pos = reward_shape(prop.arm_R.egocentric_hand_pos, target.arm_R.egocentric_hand_pos),
@@ -168,11 +82,11 @@ function compute_rewards(env::ModularImitationEnv)
             toe_joint = reward_shape(prop.foot_L.toe_angle, target.foot_L.toe_angle),
             ankle_joint = reward_shape(prop.foot_L.ankle_angle, target.foot_L.ankle_angle),
             orientation = dot(prop.foot_L.xaxis, target.foot_L.xaxis),
-            foot_pos = reward_shape(prop.foot_L.egocentric_foot_pos, target.foot_L.egocentric_foot_pos),
+            foot_pos = reward_shape(prop.foot_L.egocentric_foot_pos, target.foot_L.pos),
         ),
         leg_L = (
             knee_joint = reward_shape(prop.leg_L.knee_angle, target.leg_L.knee_angle),
-            knee_pos = reward_shape(prop.leg_L.egocentric_knee_pos, target.leg_L.egocentric_knee_pos),
+            #knee_pos = reward_shape(prop.leg_L.egocentric_knee_pos, target.leg_L.egocentric_knee_pos),
             #foot_pos = reward_shape(prop.leg_L.egocentric_foot_pos, target.leg_L.egocentric_foot_pos),
             orientation = dot(prop.foot_L.xaxis, target.foot_L.xaxis),
             hip_height = reward_shape(prop.leg_L.hip_height, target.leg_L.hip_height),
@@ -181,11 +95,11 @@ function compute_rewards(env::ModularImitationEnv)
             toe_joint = reward_shape(prop.foot_R.toe_angle, target.foot_R.toe_angle),
             ankle_joint = reward_shape(prop.foot_R.ankle_angle, target.foot_R.ankle_angle),
             orientation = dot(prop.foot_R.xaxis, target.foot_R.xaxis),
-            foot_pos = reward_shape(prop.foot_R.egocentric_foot_pos, target.foot_R.egocentric_foot_pos),
+            foot_pos = reward_shape(prop.foot_R.egocentric_foot_pos, target.foot_R.pos),
         ),
         leg_R = (
             knee_joint = reward_shape(prop.leg_R.knee_angle, target.leg_R.knee_angle),
-            knee_pos = reward_shape(prop.leg_R.egocentric_knee_pos, target.leg_R.egocentric_knee_pos),
+            #knee_pos = reward_shape(prop.leg_R.egocentric_knee_pos, target.leg_R.egocentric_knee_pos),
             #foot_pos = reward_shape(prop.leg_R.egocentric_foot_pos, target.leg_R.egocentric_foot_pos),
             orientation = dot(prop.foot_R.xaxis, target.foot_R.xaxis),
 	        hip_height = reward_shape(prop.leg_R.hip_height, target.leg_R.hip_height),
@@ -214,7 +128,7 @@ reward(env::ModularImitationEnv) = map(sum, compute_rewards(env))
 
 function status(env::ModularImitationEnv)
     prop = proprioception(env.walker)
-    target = get_current_target(env) #.proprioception
+    target = interpolated_target(env, env.target_timepoint[]) #.proprioception
 
     #if dot(prop.torso.zaxis, target.torso.zaxis) < 0.5
     #    return TERMINATED
@@ -228,7 +142,7 @@ function status(env::ModularImitationEnv)
     #    return TERMINATED
     elseif target_frame(env)+1 >= clip_length(env)
         return TRUNCATED
-    elseif norm(target.root_pose.pos - root_pos(env.walker)) > env.max_target_distance
+    elseif norm(target.root.pos - root_pos(env.walker)) > env.max_target_distance
         return TERMINATED
     else
         return RUNNING
@@ -236,25 +150,24 @@ function status(env::ModularImitationEnv)
 end
 
 function info(env::ModularImitationEnv)
-    target = get_current_target(env)
-    root_dist = norm(target.root_pose.pos - root_pos(env.walker))
-    root_ang  = 2*acos(abs(clamp(target.root_pose.quat' * root_xquat(env.walker), -1, 1)))
+    target = interpolated_target(env, env.target_timepoint[])
+    root_dist = norm(target.root.pos - root_pos(env.walker))
+    root_ang  = 2*acos(abs(clamp(target.root.rot' * root_xquat(env.walker), -1, 1)))
 
     prop = proprioception(env.walker)
-    target = target.proprioception
     (
-        info(env.walker)...,   
+        info(env.walker)...,
         lifetime = float(env.lifetime[]),
         head_pos_error   = norm(prop.head.egocentric_pos - target.head.egocentric_pos),
-        hand_L_pos_error = norm(prop.hand_L.egocentric_hand_pos - target.hand_L.egocentric_hand_pos),
-        foot_L_pos_error = norm(prop.foot_L.egocentric_foot_pos - target.foot_L.egocentric_foot_pos),
+        hand_L_pos_error = norm(prop.hand_L.egocentric_hand_pos - target.hand_L.pos),
+        foot_L_pos_error = norm(prop.foot_L.egocentric_foot_pos - target.foot_L.pos),
         finger_L_joint_err = first(prop.hand_L.finger_angle - target.hand_L.finger_angle),
         wrist_L_joint_err  = first(prop.hand_L.wrist_angle  - target.hand_L.wrist_angle),
         torso_height_above_ground = prop.torso.height_above_ground,
-        torso_z_dot = prop.torso.zaxis[3],
+        torso_z_dot = dot(prop.torso.zaxis, target.torso.zaxis),
         leg_L_hip_height = prop.leg_L.hip_height,
         leg_R_hip_height = prop.leg_R.hip_height,
-        pelvis_z_dot = dot(prop.leg_R.pelvis_zaxis, target.leg_R.pelvis_zaxis),
+        pelvis_z_dot = dot(prop.torso.pelvis_zaxis, target.torso.pelvis_zaxis),
         root_ang = root_ang,
         root_dist = root_dist,
         #cumulative_reward = env.cumulative_reward,
@@ -308,31 +221,20 @@ n_clips(env::ModularImitationEnv) = size(env.target_sensors)[3]
 target_frame(env::ModularImitationEnv) = round(Int64, env.target_timepoint[] * env.target_fps)
 target_clip(env::ModularImitationEnv) = env.target_clip[]
 
-function get_current_target(env::ModularImitationEnv)
-    precise_frame = env.target_timepoint[] * env.target_fps
-    int_frame = floor(Int64, precise_frame)
-    target = view(env.target_sensors, :, int_frame, env.target_clip[])
-    next_target = view(env.target_sensors, :, int_frame+1, env.target_clip[])
+function interpolated_target(env::ModularImitationEnv, at_time=0.0)
+    #at_time = (env.target_timepoint[] + time_ahead)
+    precise_frame = at_time * env.target_fps
+    int_frame = min(clip_length(env)-1, floor(Int64, precise_frame))
+    target =      view(env.target_sensors, :, int_frame,     env.target_clip[])
+    next_target = view(env.target_sensors, :, int_frame + 1, env.target_clip[])
     frac_frame = precise_frame - int_frame
     interpolated = (1-frac_frame) .* target .+ frac_frame .* next_target
     return interpolated
 end
 
-function get_future_root_pos(env::ModularImitationEnv, time_ahead=0.1)
-    precise_frame = (env.target_timepoint[] + time_ahead) * env.target_fps
-    int_frame = floor(Int64, precise_frame)
-    int_frame = min(clip_length(env)-1, int_frame)
-    pos_a = SVector{3}(view(env.target_sensors.root_pose.pos, :, int_frame,   env.target_clip[]))
-    pos_b = SVector{3}(view(env.target_sensors.root_pose.pos, :, int_frame+1, env.target_clip[]))
-    frac_frame = precise_frame - int_frame
-    interpolated = (1-frac_frame) .* pos_a .+ frac_frame .* pos_b
-    return interpolated
-end
-
 function precompute_target(walker::ModularRodent, orig_target::ComponentArray)
     _, dur, n_clips = size(orig_target)
-    template_prop = (root_pose = (pos=root_pos(walker), quat=root_xquat(walker)),
-                     proprioception = proprioception(walker)) |> ComponentArray
+    template_prop = target_from_pose(walker) |> ComponentArray
     new_target = ComponentArray(zeros(length(template_prop), dur, n_clips),
                                 getaxes(template_prop)[1], FlatAxis(), FlatAxis())
     @Threads.threads for i = 1:n_clips
@@ -341,12 +243,76 @@ function precompute_target(walker::ModularRodent, orig_target::ComponentArray)
             w.data.qpos .= @view orig_target.qpos[:, t, i]
             w.data.qvel .= @view orig_target.qvel[:, t, i]
             MuJoCo.forward!(w.model, w.data)
-            new_target.proprioception[:, t, i] = proprioception(w)
-            new_target.root_pose.pos[:, t, i] = root_pos(w)
-            new_target.root_pose.quat[:, t, i] = root_xquat(w)
+            new_target[:, t, i] = target_from_pose(w)
         end
     end
     return new_target
+end
+
+function target_from_pose(walker)
+    target = proprioception(walker)
+    (
+        hand_L = (
+            pos = SVector{3}(target.hand_L.egocentric_hand_pos),
+            wrist_angle = target.hand_L.wrist_angle,
+            finger_angle = target.hand_L.finger_angle,
+            xaxis = SVector{3}(target.hand_L.xaxis),
+        ),
+        arm_L = (
+            elbow_angle = target.arm_L.elbow_angle,
+            elbow_height = target.arm_L.elbow_height,
+            shoulder_height = target.arm_L.shoulder_height
+        ),
+        hand_R = (
+            pos = SVector{3}(target.hand_R.egocentric_hand_pos),
+            wrist_angle = target.hand_R.wrist_angle,
+            finger_angle = target.hand_R.finger_angle,
+            xaxis = SVector{3}(target.hand_R.xaxis),
+        ),
+        arm_R = (
+            elbow_angle = target.arm_R.elbow_angle,
+            elbow_height = target.arm_R.elbow_height,
+            shoulder_height = target.arm_R.shoulder_height
+        ),
+        foot_L = (
+            pos = SVector{3}(target.foot_L.egocentric_foot_pos),
+            toe_angle = target.foot_L.toe_angle,
+            ankle_angle = target.foot_L.ankle_angle,
+            xaxis = SVector{3}(target.foot_L.xaxis)
+        ),
+        leg_L = (
+            knee_angle = target.leg_L.knee_angle,
+            hip_height = target.leg_L.hip_height,
+        ),
+        foot_R = (
+            pos = SVector{3}(target.foot_R.egocentric_foot_pos),
+            toe_angle = target.foot_R.toe_angle,
+            ankle_angle = target.foot_R.ankle_angle,
+            xaxis = SVector{3}(target.foot_R.xaxis)
+        ),
+        leg_R = (
+            knee_angle = target.leg_R.knee_angle,
+            hip_height = target.leg_R.hip_height,
+        ),
+        torso = (
+            zaxis = SVector{3}(target.torso.zaxis),
+            pelvis_zaxis = SVector{3}(target.torso.pelvis_zaxis),
+            lumbar_bend   = target.torso.lumbar_bend,
+            lumbar_twist  = target.torso.lumbar_twist,
+            lumbar_extend = target.torso.lumbar_extend,
+            height_above_ground = target.torso.height_above_ground,
+        ),
+        head = (
+            xaxis = SVector{3}(target.head.xaxis),
+            zaxis = SVector{3}(target.head.zaxis),
+            egocentric_pos = SVector{3}(target.head.egocentric_pos),
+            mandible = target.head.mandible
+        ),
+        root = (
+            pos = root_pos(walker),
+            rot = root_xquat(walker),
+        )
+    )
 end
 
 function Base.show(io::IO, env::ModularImitationEnv)
